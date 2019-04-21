873e6f48ed47347de3f1d9ecf72ebb7044f971e2
laqatami <youseflaqatami@gmail.com>
2019-04-21 18:06:21 +0300

    new api for receiving scanned userinfo

--- coded/views.py
+++ coded/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .serializers import UserRegistrationSerializer , UserInfoSerializer , UserDataSerializer
+from .serializers import UserRegistrationSerializer , UserInfoSerializer , UserDataSerializer, FollowSerializer
 from rest_framework.generics import CreateAPIView , RetrieveUpdateAPIView , DestroyAPIView, ListAPIView ,RetrieveAPIView 
 from rest_framework.views import APIView
 from .models import UserInfo , Follow
@@ -24,6 +24,20 @@ class UserFillInfoAPIView(RetrieveUpdateAPIView):
 	lookup_fields = 'id'
 	lookup_url_kwarg = 'userinfo_id'
 	
+# create api view ()
+###
+"""
+1. define a APIView from DjangoRestFramework
+2. Create a 'get' method inside that view
+3. the method will retrieve the current logged in user  // request.user
+4. return the userinfo object relating to that user
+
+1. create a 'post' method inside the same view
+2. look at the user registration method for more clues on how to update and return a message 
+"""
+###
+
+
 
 class UserRetraiveInfoAPIView(RetrieveAPIView):
 	queryset = UserInfo.objects.all()
@@ -49,3 +63,13 @@ class FollowUserAPIView(APIView):
 			#      # follow_obj.delete()
 			# return Response({"follow": action})
 			return Response()   
+
+
+class MyContactListAPIView(ListAPIView):
+	def get_queryset(self):
+		return Follow.objects.filter(user=self.request.user)
+
+	serializer_class = FollowSerializer
+
+
+
