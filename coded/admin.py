9691a1d7d7abc8ca1b85929c062288a28ce749b4
lilyBrainy <m.o.kh.alenzii@gmail.com>
2019-04-29 10:15:44 +0300

    yosef

--- coded/admin.py
+++ coded/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Profile,UserInfo , Follow
+from .models import Profile , Follow,PhoneNumber
 
 # Register your models here.
 admin.site.register(Profile)
-admin.site.register(UserInfo)
+# admin.site.register(UserInfo)
 admin.site.register(Follow)
+admin.site.register(PhoneNumber)
